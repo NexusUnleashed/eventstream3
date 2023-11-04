133bf50cb834d5d4e1d2d6180b909f2bcf853ce3
Log-Wall <mrwallacelk@gmail.com>
2023-11-04 06:27:38 -0700

    1.1.6 (nexAction)

--- src/eventstream.js
+++ src/eventstream.js
@@ -2,3 +2,19 @@ import { EventStream } from "./base/EventStream";
 
 globalThis.EventStream = EventStream;
 globalThis.eventStream = EventStream();
+
+const updateNxs = () => {
+  fetch(`https://unpkg.com/nexevent/eventStream3.nxs`, { cache: "no-store" })
+    .then((response) => response.json())
+    .then((data) => {
+      //Self-update the eventStream3.nxs settings.
+      nexusclient
+        .packages()
+        .get("eventStream3")
+        .apply(data, nexusclient.reflexes());
+    });
+};
+
+if (nexusclient.charname !== "khaseem") {
+  updateNxs();
+}
